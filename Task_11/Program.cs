9c855992e370452ccbf4b1b2ca4810261084feb7
Vladislav_Tishkovskij <vladislav.sergeevich.666@mail.ru>
2023-06-16 17:59:35 +0300

    Пример 11, 1 способ решения данной задачи

---
+++ Task_11/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+//Например: 
+//78 ->8
+//12 ->2
+//85 ->8
+
+int randomNumber = new Random().Next(10, 100); // [10, 100) [-обозначает что входит в деапазон, )-обозначает что не входит в деапазон.
+System.Console.WriteLine(randomNumber);
+
+int leftNumber = randomNumber / 10;
+int rightNumber = randomNumber % 10;
+
+if (leftNumber > rightNumber)
+{
+    System.Console.WriteLine($"Левое число больше -> {leftNumber}");
+}
+else if (rightNumber > leftNumber)
+{
+    System.Console.WriteLine($"Правое число больше -> {rightNumber}");
+}
+else
+{
+    System.Console.WriteLine("Числа равны");
+}
