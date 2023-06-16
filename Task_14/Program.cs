1226e7732a024a707cc5aeb8a377c82bf7e12a69
Vladislav_Tishkovskij <vladislav.sergeevich.666@mail.ru>
2023-06-16 19:35:39 +0300

    Task 14, 1 способ решения задачи

---
+++ Task_14/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход число, кратно ли оно одновременно 7 и 23.
+//14 -> нет
+//46 -> нет
+//161 -> да
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 7 == 0 && num % 23 == 0)
+{
+    Console.WriteLine(num + " кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine(num + " не кратно 7 и 23");
+}
+
+Console.ReadKey();
