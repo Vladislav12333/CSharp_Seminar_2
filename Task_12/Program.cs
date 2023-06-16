feeda67c137e3acb638bb3118cfe93e023995b22
Vladislav_Tishkovskij <vladislav.sergeevich.666@mail.ru>
2023-06-16 19:18:23 +0300

    Task 12, 2 способ решения задачи

--- Task_12/Program.cs
+++ Task_12/Program.cs
@@ -2,20 +2,14 @@
 //34, 5 -> не кратно, остаток 4
 //16, 4 -> кратно
 
-Console.Write("Введите первое число: ");
-int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите второе число: ");
-int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
 
-if (num2 % num1 == 0)
+if (number1 % number2 == 0)
 {
-    Console.WriteLine(num2 + " кратно " + num1);
+    Console.WriteLine("Числа кратные");
 }
-else
-{
-    int remainder = num2 % num1;
-    Console.WriteLine(num2 + " не кратно " + num1 + ". Остаток от деления: " + remainder);
-}
-
-Console.ReadKey();
+else Console.Write("Числа не кратные. Остаток от деления равен " + number1 % number2);
